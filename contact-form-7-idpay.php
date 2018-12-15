16abc087cdf4aa8a5f06a9492187a5b5d6f16029
Iman Kiani <kianiman@gmail.com>
2018-12-15 19:03:23 +0330

    make over  activation and deactivation of the plugin into a new class

--- contact-form-7-idpay.php
+++ contact-form-7-idpay.php
@@ -14,119 +14,6 @@ require_once 'vendor/autoload.php';
 $IDPay_Callback = new IDPay\CF7\Callback();
 add_shortcode( 'idpay_cf7_result', array( $IDPay_Callback, 'handler' ) );
 
-$dir = plugin_dir_path( __FILE__ );
-register_activation_hook( __FILE__, "idpay_cf7_activate" );
-register_deactivation_hook( __FILE__, "idpay_cf7_deactivate" );
-register_uninstall_hook( __FILE__, "idpay_cf7_uninstall" );
-
-function idpay_cf7_activate() {
-	global $wpdb;
-	$table_name = $wpdb->prefix . "cf7_transactions";
-	if ( $wpdb->get_var( "show tables like '$table_name'" ) != $table_name ) {
-		$sql = "CREATE TABLE " . $table_name . " (
-			id mediumint(11) NOT NULL AUTO_INCREMENT,
-			idform bigint(11) DEFAULT '0' NOT NULL,
-			transid VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_persian_ci NOT NULL,
-			gateway VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_persian_ci NOT NULL,
-			cost bigint(11) DEFAULT '0' NOT NULL,
-			created_at bigint(11) DEFAULT '0' NOT NULL,
-			email VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_persian_ci  NULL,
-			description VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_persian_ci NOT NULL,
-			user_mobile VARCHAR(11) CHARACTER SET utf8 COLLATE utf8_persian_ci  NULL,
-			status VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_persian_ci NOT NULL,
-			PRIMARY KEY id (id)
-		);";
-		require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
-		dbDelta( $sql );
-	}
-
-	function wp_config_put( $slash = '' ) {
-		$config = file_get_contents( ABSPATH . "wp-config.php" );
-		$config = preg_replace( "/^([\r\n\t ]*)(\<\?)(php)?/i", "<?php define('WPCF7_LOAD_JS', false);", $config );
-		file_put_contents( ABSPATH . $slash . "wp-config.php", $config );
-	}
-
-	if ( file_exists( ABSPATH . "wp-config.php" ) && is_writable( ABSPATH . "wp-config.php" ) ) {
-		wp_config_put();
-	} else if ( file_exists( dirname( ABSPATH ) . "/wp-config.php" ) && is_writable( dirname( ABSPATH ) . "/wp-config.php" ) ) {
-		wp_config_put( '/' );
-	} else {
-		?>
-        <div class="error">
-            <p><?php _e( 'wp-config.php is not writable, please make wp-config.php writable - set it to 0777 temporarily, then set back to its original setting after this plugin has been activated.', 'cf7pp' ); ?></p>
-        </div>
-		<?php
-		exit;
-	}
-
-	$idpay_cf7_options = array(
-		'api_key' => '',
-		'return'  => '',
-		'sandbox' => '1',
-	);
-
-	add_option( "idpay_cf7_options", $idpay_cf7_options );
-}
-
-
-function idpay_cf7_deactivate() {
-
-	function wp_config_delete( $slash = '' ) {
-		$config = file_get_contents( ABSPATH . "wp-config.php" );
-		$config = preg_replace( "/( ?)(define)( ?)(\()( ?)(['\"])WPCF7_LOAD_JS(['\"])( ?)(,)( ?)(0|1|true|false)( ?)(\))( ?);/i", "", $config );
-		file_put_contents( ABSPATH . $slash . "wp-config.php", $config );
-	}
-
-	if ( file_exists( ABSPATH . "wp-config.php" ) && is_writable( ABSPATH . "wp-config.php" ) ) {
-		wp_config_delete();
-	} else if ( file_exists( dirname( ABSPATH ) . "/wp-config.php" ) && is_writable( dirname( ABSPATH ) . "/wp-config.php" ) ) {
-		wp_config_delete( '/' );
-	} else if ( file_exists( ABSPATH . "wp-config.php" ) && ! is_writable( ABSPATH . "wp-config.php" ) ) {
-		?>
-        <div class="error">
-            <p><?php _e( 'wp-config.php is not writable, please make wp-config.php writable - set it to 0777 temporarily, then set back to its original setting after this plugin has been deactivated.', 'cf7pp' ); ?></p>
-        </div>
-        <button onclick="goBack()">Go Back and try again</button>
-        <script>
-          function goBack() {
-            window.history.back();
-          }
-        </script>
-		<?php
-		exit;
-	} else if ( file_exists( dirname( ABSPATH ) . "/wp-config.php" ) && ! is_writable( dirname( ABSPATH ) . "/wp-config.php" ) ) {
-		?>
-        <div class="error">
-            <p><?php _e( 'wp-config.php is not writable, please make wp-config.php writable - set it to 0777 temporarily, then set back to its original setting after this plugin has been deactivated.', 'cf7pp' ); ?></p>
-        </div>
-        <button onclick="goBack()">Go Back and try again</button>
-        <script>
-          function goBack() {
-            window.history.back();
-          }
-        </script>
-		<?php
-		exit;
-	} else {
-		?>
-        <div class="error">
-            <p><?php _e( 'wp-config.php is not writable, please make wp-config.php writable - set it to 0777 temporarily, then set back to its original setting after this plugin has been deactivated.', 'cf7pp' ); ?></p>
-        </div>
-        <button onclick="goBack()">Go Back and try again</button>
-        <script>
-          function goBack() {
-            window.history.back();
-          }
-        </script>
-		<?php
-		exit;
-	}
-
-	delete_option( "idpay_cf7_options" );
-	delete_option( "idpay_cf7_my_plugin_notice_shown" );
-}
-
-
 include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
 if ( is_plugin_active( 'contact-form-7/wp-contact-form-7.php' ) ) {
 
@@ -139,8 +26,6 @@ if ( is_plugin_active( 'contact-form-7/wp-contact-form-7.php' ) ) {
 
 
 	$IDPay_CF7_Additional_settings = new IDPay\CF7\AdditionalSettingsForm();
-
-
 	add_filter( 'wpcf7_editor_panels', array(
 		$IDPay_CF7_Additional_settings,
 		'editor_panels',
@@ -152,4 +37,8 @@ if ( is_plugin_active( 'contact-form-7/wp-contact-form-7.php' ) ) {
 
 
 }
-?>
+
+
+$plugin = new IDPay\CF7\Plugin();
+register_activation_hook( __FILE__, array($plugin, 'activate') );
+register_deactivation_hook( __FILE__, array($plugin, 'deactivate') );
---
+++ includes/Plugin.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace IDPay\CF7;
+
+class Plugin {
+	public function activate() {
+		global $wpdb;
+		$table_name = $wpdb->prefix . "cf7_transactions";
+		if ( $wpdb->get_var( "show tables like '$table_name'" ) != $table_name ) {
+			$sql = "CREATE TABLE " . $table_name . " (
+			id mediumint(11) NOT NULL AUTO_INCREMENT,
+			idform bigint(11) DEFAULT '0' NOT NULL,
+			transid VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_persian_ci NOT NULL,
+			gateway VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_persian_ci NOT NULL,
+			cost bigint(11) DEFAULT '0' NOT NULL,
+			created_at bigint(11) DEFAULT '0' NOT NULL,
+			email VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_persian_ci  NULL,
+			description VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_persian_ci NOT NULL,
+			user_mobile VARCHAR(11) CHARACTER SET utf8 COLLATE utf8_persian_ci  NULL,
+			status VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_persian_ci NOT NULL,
+			PRIMARY KEY id (id)
+		);";
+			require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+			dbDelta( $sql );
+		}
+
+		function wp_config_put( $slash = '' ) {
+			$config = file_get_contents( ABSPATH . "wp-config.php" );
+			$config = preg_replace( "/^([\r\n\t ]*)(\<\?)(php)?/i", "<?php define('WPCF7_LOAD_JS', false);", $config );
+			file_put_contents( ABSPATH . $slash . "wp-config.php", $config );
+		}
+
+		if ( file_exists( ABSPATH . "wp-config.php" ) && is_writable( ABSPATH . "wp-config.php" ) ) {
+			wp_config_put();
+		} else if ( file_exists( dirname( ABSPATH ) . "/wp-config.php" ) && is_writable( dirname( ABSPATH ) . "/wp-config.php" ) ) {
+			wp_config_put( '/' );
+		} else {
+			?>
+			<div class="error">
+				<p><?php _e( 'wp-config.php is not writable, please make wp-config.php writable - set it to 0777 temporarily, then set back to its original setting after this plugin has been activated.', 'cf7pp' ); ?></p>
+			</div>
+			<?php
+			exit;
+		}
+
+		$idpay_cf7_options = array(
+			'api_key' => '',
+			'return'  => '',
+			'sandbox' => '1',
+		);
+
+		add_option( "idpay_cf7_options", $idpay_cf7_options );
+	}
+
+	function deactivate() {
+
+		function wp_config_delete( $slash = '' ) {
+			$config = file_get_contents( ABSPATH . "wp-config.php" );
+			$config = preg_replace( "/( ?)(define)( ?)(\()( ?)(['\"])WPCF7_LOAD_JS(['\"])( ?)(,)( ?)(0|1|true|false)( ?)(\))( ?);/i", "", $config );
+			file_put_contents( ABSPATH . $slash . "wp-config.php", $config );
+		}
+
+		if ( file_exists( ABSPATH . "wp-config.php" ) && is_writable( ABSPATH . "wp-config.php" ) ) {
+			wp_config_delete();
+		} else if ( file_exists( dirname( ABSPATH ) . "/wp-config.php" ) && is_writable( dirname( ABSPATH ) . "/wp-config.php" ) ) {
+			wp_config_delete( '/' );
+		} else if ( file_exists( ABSPATH . "wp-config.php" ) && ! is_writable( ABSPATH . "wp-config.php" ) ) {
+			?>
+			<div class="error">
+				<p><?php _e( 'wp-config.php is not writable, please make wp-config.php writable - set it to 0777 temporarily, then set back to its original setting after this plugin has been deactivated.', 'cf7pp' ); ?></p>
+			</div>
+			<button onclick="goBack()">Go Back and try again</button>
+			<script>
+              function goBack() {
+                window.history.back();
+              }
+			</script>
+			<?php
+			exit;
+		} else if ( file_exists( dirname( ABSPATH ) . "/wp-config.php" ) && ! is_writable( dirname( ABSPATH ) . "/wp-config.php" ) ) {
+			?>
+			<div class="error">
+				<p><?php _e( 'wp-config.php is not writable, please make wp-config.php writable - set it to 0777 temporarily, then set back to its original setting after this plugin has been deactivated.', 'cf7pp' ); ?></p>
+			</div>
+			<button onclick="goBack()">Go Back and try again</button>
+			<script>
+              function goBack() {
+                window.history.back();
+              }
+			</script>
+			<?php
+			exit;
+		} else {
+			?>
+			<div class="error">
+				<p><?php _e( 'wp-config.php is not writable, please make wp-config.php writable - set it to 0777 temporarily, then set back to its original setting after this plugin has been deactivated.', 'cf7pp' ); ?></p>
+			</div>
+			<button onclick="goBack()">Go Back and try again</button>
+			<script>
+              function goBack() {
+                window.history.back();
+              }
+			</script>
+			<?php
+			exit;
+		}
+
+		delete_option( "idpay_cf7_options" );
+	}
+}
